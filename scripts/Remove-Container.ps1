07752860a9152213ed542736656623ef7ac0f29a
Gunnar Gestsson <gunnar@navision.guru>
2021-01-04 09:22:26 +0000

    Update build scripts

--- scripts/Remove-Container.ps1
+++
@@ -1,9 +0,0 @@
-﻿Param(
-    [ValidateSet('AzureDevOps','Local','AzureVM')]
-    [string] $buildEnv = "AzureDevOps",
-
-    [Parameter(Mandatory=$false)]
-    [string] $containerName = $ENV:CONTAINERNAME
-)
-
-Remove-BCContainer -containerName $containerName
